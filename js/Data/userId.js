833b032e6989dbcb0a5a8832a9b271ef34d550e9
Fabian Salas <fabian.salas.astete@gmail.com>
2023-07-03 18:01:35 -0400

    m

--- js/Data/userId.js
+++ js/Data/userId.js
@@ -1,4 +1,4 @@
-export const PropertyData = {CodigoUsuarioMaestro: 0, companyId: 1, realtorId:0};
+export const PropertyData = {CodigoUsuarioMaestro:10075, companyId: 10087, realtorId:0};
 
 export const limitDataApi = { limit:6 };
 
